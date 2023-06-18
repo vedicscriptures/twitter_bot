1028bcb35708e5f98f6bc7f8af7a7a670623b126
Pt. Prashant tripathi <26687933+PtPrashantTripathi@users.noreply.github.com>
2023-06-18 09:33:20 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -6,19 +6,26 @@ import os
 Slok = Slokm()
 
 # Tweet Text limit
-Post = (Slok[:277] + '..') if len(Slok) > 280 else Slok
+Post = (Slok[:277] + "..") if len(Slok) > 280 else Slok
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler(os.environ["APIKey"],os.environ["APISecretKey"])
-auth.set_access_token(os.environ["AccessToken"],os.environ["AccessTokenSecret"])
-api = tweepy.API(auth)
+consumer_key = os.environ["APIKey"]
+consumer_secret = os.environ["APISecretKey"]
+access_token = os.environ["AccessToken"]
+access_token_secret = os.environ["AccessTokenSecret"]
+
+client = tweepy.Client(
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret,
+)
 
 # Tweet Posting
 try:
-    api.verify_credentials()
-    print("Authentication OK\n")
-    api.update_status(Post)
+    status = client.create_tweet(text=Post)
     print(Post)
+    print(status)
     print("\nPosted")
 except Exception as error:
     print(f"Error during authentication :\n{error}")
