985aa203d77a4a2ecfd33054eb7ccf074ad15fbf
Pt. Prashant tripathi <26687933+PtPrashantTripathi@users.noreply.github.com>
2023-07-28 08:57:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,25 @@ import tweepy
 from bgapi import *
 import os
 
+def generate_bearer_token(api_key, api_secret_key):
+    # URL for Twitter API endpoint to obtain the bearer token
+    url = "https://api.twitter.com/oauth2/token"
+
+    # Set up the HTTP Basic Authentication header with API key and API secret key
+    auth_headers = {
+        "Authorization": f"Basic {base64.b64encode(f'{api_key}:{api_secret_key}'.encode()).decode()}"
+    }
+
+    # Parameters for the POST request to obtain the bearer token
+    data = {"grant_type": "client_credentials"}
+
+    # Make the POST request to get the bearer token
+    response = requests.post(url, headers=auth_headers, data=data)
+
+    # Parse the response JSON to extract the bearer token
+    bearer_token = response.json().get("access_token")
+
+    return bearer_token
 
 def main():
     # Getting sloks from Bhagavad Gita API
@@ -16,7 +35,8 @@ def main():
     api_secret = os.environ["APISecretKey"]
     access_token = os.environ["AccessToken"]
     access_token_secret = os.environ["AccessTokenSecret"]
-    bearer_token = os.environ["BearerToken"]
+    # bearer_token = os.environ["BearerToken"]
+    bearer_token = generate_bearer_token(api_key, api_secret_key)
 
     client = tweepy.Client(
         bearer_token, api_key, api_secret, access_token, access_token_secret
